202571c3d49f87da5c9d28ce90b519d8946e4297
Henri Heyden <44402515+WiiXLinux@users.noreply.github.com>
2021-06-28 18:20:41 +0200

    Add files via upload

---
+++ src/main/java/jade/LevelScene.java
@@ -0,0 +1,15 @@
+package jade;
+
+import java.awt.event.KeyEvent;
+
+public class LevelScene extends Scene {
+    public LevelScene(){
+        System.out.println("Inside level Scene");
+    }
+
+
+    @Override
+    public void update(float dt) {
+
+    }
+}
